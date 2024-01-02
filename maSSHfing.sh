30a9995f8c771d24435b00ed63d130674b98841d
rapbando <rapbando@hotmail.it>
2024-01-02 18:32:23 +0000

    Update maSSHfing.sh

--- maSSHfing.sh
+++ maSSHfing.sh
@@ -1,16 +1,13 @@
 #!/bin/sh
 
-
  masscan -p22 $1 -oG session.masshfing  --rate=1000   
   if [[ $? -ne 0 ]] ; then
       exit 1
   fi
  grep -oP '(?<=Host: )\S*' session.masshfing >openports.masshfing 
- printf "[+] Parsing fingerprints while connecting to SSH servers...\n------------------------------------------------------------------------------------\n"
+ printf "[i] Parsing fingerprints while connecting to SSH servers...\n------------------------------------------------------------------------------------\n"
  echo
-
  rm session.masshfing 
- 
  file=openports.masshfing
  while read line ; do
     python3 hassh/python/hassh.py -i eth0 -fp server -l json -o masshfing.log.json &
@@ -18,9 +15,8 @@
     ssh -T -o StrictHostKeyChecking=yes admin@$ip 
     sleep 2
     printf  "\n------------------------------------------------------------------------------------\n"
-    
  done < ${file}
  echo All done!
  COUNT=$(grep -c ".*" openports.masshfing)  
  echo -n "[i]" $COUNT "ENTRIES SAVED TO: masshfing.log.json"	       
- echo    
+ echo 
