f59b3bd2a71d424f1eba3fbbc62af1f9af0a6b99
Lawrence <rapbando@hotmail.it>
2025-07-30 22:04:12 +0000

    Update maSSHfing.sh

--- maSSHfing.sh
+++ maSSHfing.sh
@@ -1,22 +1,22 @@
 #!/bin/sh
 
- masscan -p22 $1 -oG session.masshfing  --rate=1000   
+ masscan -p22 $1 -oG session.masshfing  --rate=1000
   if [[ $? -ne 0 ]] ; then
       exit 1
   fi
- grep -oP '(?<=Host: )\S*' session.masshfing >openports.masshfing 
+ grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}' session.masshfing > openports.masshfing
  printf "[i] Parsing fingerprints while connecting to SSH servers...\n------------------------------------------------------------------------------------\n"
  echo
  rm session.masshfing 
  file=openports.masshfing
  while read line ; do
-    python3 hassh/python/hassh.py -i eth0 -fp server -l json -o masshfing.log.json &
+    python3 hassh/python/hassh.py -i eth0 -fp server -l json -o masshfing.log.json --print &
     ip=$( echo "$line" | cut -d ' ' -f 1 )
-    ssh -T -o StrictHostKeyChecking=yes admin@$ip 
     sleep 2
+    ssh -T -o StrictHostKeyChecking=yes admin@$ip
     printf  "\n------------------------------------------------------------------------------------\n"
  done < ${file}
  echo All done!
  COUNT=$(grep -c ".*" openports.masshfing)  
- echo -n "[i]" $COUNT "ENTRIES SAVED TO: masshfing.log.json"	       
- echo 
+ echo -n "[i]" $COUNT "ENTRIES SAVED TO: masshfing.log.json"
+ echo
